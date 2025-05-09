f27738b7e4c84d2a2ee958580631fd6a6139da30
MunirHashimi <munirxkhan50@gmail.com>
2025-05-09 20:23:41 +0430

    files added

---
+++ script.js
@@ -0,0 +1,14 @@
+const slider = document.getElementById('slider');
+const overlay = document.getElementById('overlay');
+const sliderLine = document.getElementById('slider-line');
+
+slider.addEventListener('mousemove', (e) => {
+    const rect = slider.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+
+    if (x < 0) x = 0;
+    if (x > rect.width) x = rect.width;
+
+    overlay.style.width = x + 'px';
+    sliderLine.style.left = x + 'px';
+});
